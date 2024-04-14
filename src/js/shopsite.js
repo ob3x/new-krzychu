4cc5ba28e504d9275bcddd6db71b201d6ad45a1d
Szymon <ob3x.biznes@gmail.com>
2024-04-14 16:08:19 +0200

    update

--- src/js/shopsite.js
+++ src/js/shopsite.js
@@ -2,6 +2,9 @@ const rangeInput = document.querySelectorAll('.shopsite__price-range input'),
 	priceInput = document.querySelectorAll('.shopsite__price-box--item'),
 	progress = document.querySelector('.shopsite__price-slider--item');
 
+const colors = document.querySelectorAll('.shopsite-left__colors-item');
+const sizesBtn = document.querySelectorAll('.shopsite__sizes-item');
+const hearsBtn = document.querySelectorAll('.shopsite-right__item-heart');
 let priceGap = 250;
 
 priceInput.forEach((input) => {
@@ -9,7 +12,7 @@ priceInput.forEach((input) => {
 		let minVal = parseInt(priceInput[0].value),
 			maxVal = parseInt(priceInput[1].value);
 
-		if((maxVal - minVal >= priceGap) && maxVal <= 2000) {
+		if (maxVal - minVal >= priceGap && maxVal <= 2000) {
 			if (e.target.classList.contains('min-input')) {
 				rangeInput[0].value = minVal;
 				progress.style.left = (minVal / rangeInput[0].max) * 100 + '%';
@@ -20,7 +23,6 @@ priceInput.forEach((input) => {
 		}
 	});
 });
-
 
 rangeInput.forEach((input) => {
 	input.addEventListener('input', (e) => {
@@ -39,5 +41,33 @@ rangeInput.forEach((input) => {
 			progress.style.left = (minVal / rangeInput[0].max) * 100 + '%';
 			progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + '%';
 		}
+	});
+});
+
+const changeIcon = (e) => {
+	sizesBtn.forEach((size) => {
+		size.classList.remove('sizes-active');
+	});
+	e.target.classList.toggle('sizes-active');
+};
+
+const changeColor = (e) => {
+	colors.forEach((color) => {
+		color.classList.remove('color-active');
+	});
+	e.target.classList.add('color-active');
+};
+
+sizesBtn.forEach((btn) => {
+	btn.addEventListener('click', changeIcon);
+});
+
+colors.forEach((color) => {
+	color.addEventListener('click', changeColor);
+});
+
+hearsBtn.forEach((heart) => {
+	heart.addEventListener('click', () => {
+		heart.classList.toggle('heart-red');
 	});
 });
